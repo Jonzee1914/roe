35992b46d39e9984f5b42b77c615b7e566443cd1
Phillip MacDonald <pmacdonald2015@yahoo.com>
2022-03-23 14:47:48 -0400

    test

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,10 +5,10 @@ db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
+    { name: 'Land Creatures' },
+    { name: 'Flying Creatures' },
+    { name: 'Sea Creatures' },
+    { name: 'Cryptids' },
     { name: 'Toys' }
   ]);
 
@@ -18,12 +18,12 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Lochness',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'cookie-tin.jpg',
       category: categories[0]._id,
-      price: 2.99,
+      price: 22414.99,
       quantity: 500
     },
     {
@@ -36,25 +36,25 @@ db.once('open', async () => {
       quantity: 500
     },
     {
-      name: 'Toilet Paper',
+      name: 'Skunk Apes',
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
+        'Skunk Apes',
       image: 'toilet-paper.jpg',
-      price: 7.99,
+      price: 710.99,
       quantity: 20
     },
     {
-      name: 'Handmade Soap',
+      name: 'Chupacabra',
       category: categories[1]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
+        'Chupacabra.',
       image: 'soap.jpg',
-      price: 3.99,
+      price: 3363.99,
       quantity: 50
     },
     {
-      name: 'Set of Wooden Spoons',
+      name: 'Bigfoot',
       category: categories[1]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
@@ -63,7 +63,7 @@ db.once('open', async () => {
       quantity: 100
     },
     {
-      name: 'Camera',
+      name: 'Dragon',
       category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
@@ -72,7 +72,7 @@ db.once('open', async () => {
       quantity: 30
     },
     {
-      name: 'Tablet',
+      name: 'Gryffin',
       category: categories[2]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
