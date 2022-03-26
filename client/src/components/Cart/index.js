96a59eed29c378724419fb8e1c60d3f26cd7a9b9
Erik Jones <jonzee1914@gmail.com>
2022-03-25 20:26:11 -0400

    replaced strip key

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -9,7 +9,7 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_51KgcbvHLMgOHiK2LjCNGSgPpTOY1ou0w8X7B5nvIM2T3S8qihQXIGufRqrDOwNhBVMsSeps2HNMtAUXTOttWqvkx00OOkxCAp4');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe = require('stripe')('sk_test_51KgcbvHLMgOHiK2LYDgSLUicf2nZyUpueLRS84f2SejTwyZ8ZLcYnYfHlOQAlcM2IMbAephpkfGa1wW4OFRkW6L600TLq1ij8U');
 
 const resolvers = {
   Query: {
